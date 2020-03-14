50bf04e3a65b755f8f473f57a0a2146f65abd6a1
tintheturtle <49880779+tintheturtle@users.noreply.github.com>
2020-03-14 16:56:27 -0400

    Updates

--- precache-manifest.b169eb0817e4c79f8f68a860358291e8.js
+++ precache-manifest.b169eb0817e4c79f8f68a860358291e8.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4377bc946c6d350cd0270cfc69c334a8",
+    "revision": "aac170dae3e481c5dce5a4199399e00d",
     "url": "/personal-website/index.html"
   },
   {
-    "revision": "8094669a21397f18110c",
+    "revision": "55911e3bb79cbb379b89",
     "url": "/personal-website/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "79735cc24c9948c19081",
-    "url": "/personal-website/static/css/main.966f73cc.chunk.css"
+    "revision": "292b446d9f0f0a077cde",
+    "url": "/personal-website/static/css/main.077de762.chunk.css"
   },
   {
-    "revision": "8094669a21397f18110c",
-    "url": "/personal-website/static/js/2.68e09f22.chunk.js"
+    "revision": "55911e3bb79cbb379b89",
+    "url": "/personal-website/static/js/2.c6b76adb.chunk.js"
   },
   {
     "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
-    "url": "/personal-website/static/js/2.68e09f22.chunk.js.LICENSE.txt"
+    "url": "/personal-website/static/js/2.c6b76adb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "79735cc24c9948c19081",
-    "url": "/personal-website/static/js/main.467e5523.chunk.js"
+    "revision": "292b446d9f0f0a077cde",
+    "url": "/personal-website/static/js/main.53f0045d.chunk.js"
   },
   {
     "revision": "60431df29b2c61545076",
@@ -32,8 +32,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/personal-website/static/media/Example.568bfcd9.png"
   },
   {
+    "revision": "0918d78648457def080137b179fc5608",
+    "url": "/personal-website/static/media/github.0918d786.png"
+  },
+  {
     "revision": "5f1d49ca7982176d3c2b193d00ce5b12",
     "url": "/personal-website/static/media/headshot.5f1d49ca.jpg"
+  },
+  {
+    "revision": "5fcc89b862d7965249fee81f094fa194",
+    "url": "/personal-website/static/media/instagram.5fcc89b8.png"
   },
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.59320b42b12f2fb7bf7287e1b8aa1614.js"
+  "/personal-website/precache-manifest.b169eb0817e4c79f8f68a860358291e8.js"
 );
 
 self.addEventListener('message', (event) => {
