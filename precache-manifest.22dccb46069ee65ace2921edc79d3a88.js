368177757848bd72e8ec2402ffd9c7567a92efd3
tintheturtle <49880779+tintheturtle@users.noreply.github.com>
2020-03-14 14:08:57 -0400

    Updates

--- precache-manifest.22dccb46069ee65ace2921edc79d3a88.js
+++ precache-manifest.22dccb46069ee65ace2921edc79d3a88.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5b99fa6b4bd7b5026ee5590fa21144f8",
+    "revision": "2f559b778e09764e6f1f826960c9ad69",
     "url": "/personal-website/index.html"
   },
   {
-    "revision": "510eade436443187dbf8",
+    "revision": "8094669a21397f18110c",
     "url": "/personal-website/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "5c64c9b3d4d05068f249",
-    "url": "/personal-website/static/css/main.cae52df5.chunk.css"
+    "revision": "c8866009e37e6f1b8bc0",
+    "url": "/personal-website/static/css/main.6ba4cd3d.chunk.css"
   },
   {
-    "revision": "510eade436443187dbf8",
-    "url": "/personal-website/static/js/2.55ee5f47.chunk.js"
+    "revision": "8094669a21397f18110c",
+    "url": "/personal-website/static/js/2.68e09f22.chunk.js"
   },
   {
     "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
-    "url": "/personal-website/static/js/2.55ee5f47.chunk.js.LICENSE.txt"
+    "url": "/personal-website/static/js/2.68e09f22.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5c64c9b3d4d05068f249",
-    "url": "/personal-website/static/js/main.6a1fc116.chunk.js"
+    "revision": "c8866009e37e6f1b8bc0",
+    "url": "/personal-website/static/js/main.467e5523.chunk.js"
   },
   {
     "revision": "60431df29b2c61545076",
@@ -38,5 +38,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
     "url": "/personal-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3dcb5f7c8f4e9cf626af921379961f86",
+    "url": "/personal-website/static/media/pet.3dcb5f7c.png"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.58a350e130fdc550f56ba77cccd05a0f.js"
+  "/personal-website/precache-manifest.22dccb46069ee65ace2921edc79d3a88.js"
 );
 
 self.addEventListener('message', (event) => {
